8c563d5bd662a0d26c4d5ca2554e23b6306d9d47
cshay1985 <93575558+cshay1985@users.noreply.github.com>
2024-03-18 11:59:11 +0200

    Update index.js to common js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {
+const {
   config,
   cacheConfig,
   stats,
@@ -11,4 +11,19 @@ export {
   registerInterceptor,
   getAddress,
   backgroundRefresh,
-} from './axios-cached-dns-resolve.js'
+} = require('./axios-cached-dns-resolve.js');
+
+module.exports = {
+  config,
+  cacheConfig,
+  stats,
+  init,
+  reset,
+  startBackgroundRefresh,
+  startPeriodicCachePrune,
+  getStats,
+  getDnsCacheEntries,
+  registerInterceptor,
+  getAddress,
+  backgroundRefresh
+}
